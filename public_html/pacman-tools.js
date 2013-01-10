f866c2909bbc9527c20c2c8bba900f52992a06c5
Heikki Haapala <hhaapala@gmail.com>
2013-01-10 10:05:18 +0200

    Update public_html/pacman-tools.js

--- public_html/pacman-tools.js
+++ public_html/pacman-tools.js
@@ -1,6 +1,7 @@
 // "toolset"
 // helper functions
 pacman.tools = (function() {
+    // returns movement in x and y directions: 0 = right, 1 = down, 2 = left, 3 = up
     function getMovement(direction) {
         switch (direction) {
             case 0:
@@ -14,12 +15,12 @@ pacman.tools = (function() {
         }
     }
 
-    // Euclidean distance between two points
+    // euclidean distance between two points
     function distanceBetween(x1, y1, x2, y2) {
         return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
     }
 
-    // Gives position in grid by pixel position
+    // gives position in grid by pixel position
     function getTilePosition(position) {
         return {
             row: Math.floor((position.y + pacman.config.tileSize / 2) / pacman.config.tileSize),
@@ -27,15 +28,22 @@ pacman.tools = (function() {
         };
     }
 
+    // ascending sorter
     function sortByDistance(a, b) {
         return a.distance - b.distance;
     }
     
+    // resets positions and movements to start conditions
     function reset(object) {
         object.position = object.originalStart;
         object.movement = {x: -1, y: 0, objectRotation: 180};
+        // special for player
         if (object.newMovement !== undefined) {
             object.newMovement = {x: -1, y: 0, objectRotation: 180};
+        }
+        // special for ghost
+        if (object.mode !== undefined) {
+            object.mode = "out";
         }
     }
 
